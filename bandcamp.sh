d141659c1276548c5aa69f0e9422ef46a1fb2821
knm4x <max.hampel@gmail.com>
2023-04-10 23:44:38 +0200

    initial commit

---
+++ bandcamp.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+/usr/local/Cellar/openjdk/19.0.2/bin/java -jar /Volumes/T7/bandcamp-collection-downloader.jar -c=/Volumes/T7/cookies.json -f=flac -d=/Volumes/T7/music_lossless_bandcamp knmx
+IFS=$'\n'
+paths=($(find /Volumes/T7 -name "*.flac"))
+unset IFS
+
+#printf "%s\n" "${paths[@]}"
+for i in "${paths[@]}"
+do
+	echo "$i"
+	opath="${i%/*}/"
+	xld -o "$opath" -f aif $i
+	rm $i
+done
+
