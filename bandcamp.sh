07f5d82928d2f3acd237c364184fa82ec880fa64
knm4x <max.hampel@gmail.com>
2023-10-13 21:07:45 +0200

    not safe but better

--- bandcamp.sh
+++ bandcamp.sh
@@ -8,13 +8,19 @@
 
 # requires bandcamp cookies.json in same directory as script
 # see https://github.com/Ezwen/bandcamp-collection-downloader
-
+xld
+if [ $? -ne 0 ]; then
+  echo "xld not installed, please run brew install xld and start it once to give execution permission"
+  exit 1
+fi
 
 # set working directory to script location
 cd "$(dirname "$0")"
 
 # dl new files in flac format to /Volumes/T7/music_lossless_bandcamp
 java -jar bandcamp-collection-downloader.jar -c=cookies.json -f=flac -d=/Volumes/T7/music_lossless_bandcamp knmx
+
+# test if xld is working to prevent accidental deletion without conversion
 
 
 # find every flac in /Volumes/T7 and convert to aiff
@@ -28,9 +34,6 @@ do
 	echo "$i"
 	opath="${i%/*}/"
 	xld -o "$opath" -f aif $i
-	if [ $? -ne 0 ]; then
-  		exit 1
-	fi
 	rm $i
 done
 
